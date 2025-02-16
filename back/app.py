b5ca04ac05e8e82b956ca3cfe4ae3163e66736d0
Uday Bhaskar Mathangi <114668552+UB2002@users.noreply.github.com>
2025-02-16 20:01:30 +0530

    Update app.py

--- back/app.py
+++ back/app.py
@@ -132,7 +132,7 @@ def log_request(response):
 
 # Run Flask App
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
 
 
 #################################################################################
